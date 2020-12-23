a22e112d2ead6b5cb7d4a2521a2079388d13fd4b
zzz <2395436684@qq.com>
2020-12-23 23:32:24 +0800

    1223

--- src/components/HeaderDropdown/index.jsx
+++ src/components/HeaderDropdown/index.jsx
@@ -5,7 +5,7 @@ import styles from './index.less';
 
 const HeaderDropdown = ({ overlayClassName: cls, ...restProps }) => 
 {
-  console.log({ overlayClassName: cls, ...restProps }); // ...restProps  包含了父组件的其他参数
+  // console.log({ overlayClassName: cls, ...restProps }); // ...restProps  包含了父组件的其他参数
   return (
     <Dropdown overlayClassName={classNames(styles.container, cls)} {...restProps} />
   )
